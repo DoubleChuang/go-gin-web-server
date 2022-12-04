b6369aa5d931533ae3d7fa9793de8e24f6e3d5a3
DoubleChuang <ethan9141@gmail.com>
2022-12-04 21:49:11 +0800

    fix: remove unused

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 
 func main() {
 	ConfigRuntime()
-	StartWorkers()
 	StartGin()
 }
 
@@ -22,27 +21,18 @@ func ConfigRuntime() {
 	fmt.Printf("Running with %d CPUs\n", nuCPU)
 }
 
-// StartWorkers start starsWorker by goroutine.
-func StartWorkers() {
-	go statsWorker()
-}
-
 // StartGin starts gin web server with setting router.
 func StartGin() {
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.New()
-	router.Use(rateLimit, gin.Recovery())
 	router.LoadHTMLGlob("resources/*.html")
 	router.Static("/static", "resources/static")
 	router.GET("/", GetIndex)
-	router.GET("/room/:roomid", roomGET)
-	router.POST("/room-post/:roomid", roomPOST)
-	router.GET("/stream/:roomid", streamRoom)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "80"
 	}
 	fmt.Printf("Port %s\n", port)
 	if err := router.Run(":" + port); err != nil {
